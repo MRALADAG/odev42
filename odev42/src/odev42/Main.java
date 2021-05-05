29e15e0b7e1937ce0117bc445a23a588400d7edd
MRALADAG <mstf_aladag@yahoo.com>
2021-05-05 21:41:12 +0300

    Several codes updated.

--- odev42/src/odev42/Main.java
+++ odev42/src/odev42/Main.java
@@ -3,7 +3,13 @@ package odev42;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Player player = new Player(1, "Engin", "Demiroð", 1985, "3697741564584");
+
+		PlayerManager playerManager = new PlayerManager(new MernisCustomerCheckService(),
+				new SqlServerDataBaseManager());
+
+		playerManager.add(player);
 
 	}
 
