bc4c226caba58572e460c63e7da71fefa7001eea
MRALADAG <mstf_aladag@yahoo.com>
2021-05-15 23:50:11 +0300

    Güncellemeler yapıldı.

--- odev42/src/odev42/MernisCustomerCheckService.java
+++ odev42/src/odev42/MernisCustomerCheckService.java
@@ -4,7 +4,7 @@ public class MernisCustomerCheckService implements IPlayerValidationService {
 
 	@Override
 	public boolean customerCheckIfPerson(Player player) {
-		// TODO Auto-generated method stub
+		
 		return true;
 	}
 
